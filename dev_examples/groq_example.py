20456473a863732dc0759c1df987bafbb8c20228
tamte1 <tamte1@stolaf.edu>
2024-09-30 10:54:41 -0400

    example of using groq api free tier

--- dev_examples/groq_example.py
+++ dev_examples/groq_example.py
@@ -1,10 +1,10 @@
 from groq import Groq
 import os
+from dotenv import load_dotenv
 
+load_dotenv("../.env")
 
-client = Groq(
-    api_key=os.environ.get("GROQ_API_KEY"),
-)
+client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
 
 if __name__ == '__main__':
     chat_completion = client.chat.completions.create(
@@ -18,3 +18,4 @@ if __name__ == '__main__':
     )
 
     print(chat_completion.choices[0].message.content)
+
