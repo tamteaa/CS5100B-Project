e79b6477a622edbc564b6abdf2d988ba8c2024b4
tamte1 <tamte1@stolaf.edu>
2024-09-30 11:15:49 -0400

    updated groq_example.py, added some new directories

--- examples/groq_example.py
+++ examples/groq_example.py
@@ -2,8 +2,14 @@ from groq import Groq
 import os
 from dotenv import load_dotenv
 
+# 1. go to groq.com and generate an api key
+# 2. create a .env file in the root directory
+# 3. put the api key in .env file (GROQ_API_KEY = xxxx)
+
+# load the GROQ API KEY from a .env file
 load_dotenv("../.env")
 
+# initialize a groq client (get an api key from their website)
 client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
 
 if __name__ == '__main__':
