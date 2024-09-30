ea2068dda018ef053cc909d6e638b6d52e5960c6
tamte1 <tamte1@stolaf.edu>
2024-09-30 10:50:53 -0400

    test env not committed

---
+++ dev_examples/groq_example.py
@@ -0,0 +1,20 @@
+from groq import Groq
+import os
+
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+if __name__ == '__main__':
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": "Explain the importance of fast language models",
+            }
+        ],
+        model="llama3-8b-8192",
+    )
+
+    print(chat_completion.choices[0].message.content)
