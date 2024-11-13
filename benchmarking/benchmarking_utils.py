c2d3debfb8c1352902f26ea98bcb5d96dbdfcda2
tamte1 <tamte1@stolaf.edu>
2024-11-13 10:52:29 -0500

    fixed table.py, made relevant changes to envManager.py

--- benchmarking/benchmarking_utils.py
+++ benchmarking/benchmarking_utils.py
@@ -1,7 +1,9 @@
 import random
 
+
 def get_random_goal_position(grid_size):
     return random.randint(0, grid_size[0] - 1), random.randint(0, grid_size[1] - 1)
+
 
 def get_random_start_positions(num_agents, grid_size, goal_pos):
     positions = set()
