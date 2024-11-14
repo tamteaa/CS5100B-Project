0e4ae5dbd795120d1aa38df50083458d68dbb906
sdevanathan96 <devanathan.s@northeastern.edu>
2024-11-14 10:56:16 -0500

    Created benchmarking scenarios for single agent and multiagent navigation.

--- benchmarking/benchmarking_utils.py
+++ benchmarking/benchmarking_utils.py
@@ -9,7 +9,7 @@ def get_random_start_positions(num_agents, grid_size, goal_pos):
     positions = set()
     while len(positions) < num_agents:
         pos = (random.randint(0, grid_size[0] - 1), random.randint(0, grid_size[1] - 1))
-        while pos == goal_pos:
+        while pos == goal_pos:  # Ensure start position is not the goal position
             pos = (random.randint(0, grid_size[0] - 1), random.randint(0, grid_size[1] - 1))
         positions.add(pos)
     return list(positions)
